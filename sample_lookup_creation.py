481937701a549940be9136b1a701ac0e90c93cca
tnakash <antarctica219@gmail.com>
2025-01-15 13:44:54 +0900

    add rainfall folder

--- sample_lookup_creation.py
+++ sample_lookup_creation.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # CSVファイルの読み込み
-input_csv = 'rainfall_data_2000_2100_climate_change.csv'  # CSVファイル名
-output_txt = 'rainfall_lookup_climate_change.txt'  # Vensim用のLOOKUPデータ出力ファイル
+input_csv = 'rainfall/rainfall_data_2000_2100_climate_change.csv'  # CSVファイル名
+output_txt = 'rainfall/rainfall_lookup_climate_change.txt'  # Vensim用のLOOKUPデータ出力ファイル
 
 # CSVデータの読み込み
 df = pd.read_csv(input_csv)
