f1b9d138a97585eea182cada2fe9ffd2d60be98c
tnakash <antarctica219@gmail.com>
2025-02-08 13:33:01 +0900

    divide flood and innundation

--- src/sample_lookup_creation.py
+++ src/sample_lookup_creation.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 # CSVファイルの読み込み
-input_csv = '../rainfall/rainfall_data_2000_2100_climate_change.csv'  # CSVファイル名
-output_txt = '../rainfall/rainfall_lookup_climate_change.txt'  # Vensim用のLOOKUPデータ出力ファイル
+input_csv = '../rainfall/daily_prec_gifu.csv'  # CSVファイル名
+output_txt = '../rainfall/daily_prec_gifu.txt'  # Vensim用のLOOKUPデータ出力ファイル
 
 # CSVデータの読み込み
 df = pd.read_csv(input_csv)
 
 # 日付から日数（タイムステップ）を計算
-df['Time'] = pd.to_datetime(df['Date']).sub(pd.Timestamp("2000-01-01")).dt.days + 1
+df['Time'] = pd.to_datetime(df['Date']).sub(pd.Timestamp("1990/1/1")).dt.days + 1
 
 # LOOKUP形式の生成
 lookup_values = []
